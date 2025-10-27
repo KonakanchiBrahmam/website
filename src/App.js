2db3fa6d9a0530cc643f1856f7276bfb646accb9
KonakanchiBrahmam <konakanchibrahmam2151@gmail.com>
2025-10-27 16:31:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
 
 const App = () => {
-    return (
-        <div>
-            <Header />
-            {/* Add your main content here */}
-            <Footer />
-        </div>
-    );
+  return <h1>Hello React!</h1>;
 };
 
 export default App;
