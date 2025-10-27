0faced913b703cf516c834b292c8e2fc234446fe
KonakanchiBrahmam <konakanchibrahmam2151@gmail.com>
2025-10-27 16:49:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
 
 const App = () => {
-  return <h1>Hello React!</h1>;
+  return (
+    <div>
+      <Header />
+
+      {/* 🧱 Main content section */}
+      <main style={{ marginTop: '80px' }}>
+        <div className="container text-center py-5">
+          <h1>Welcome to Rhyno EV</h1>
+          <p>Experience the next generation of electric vehicles.</p>
+        </div>
+      </main>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
